8d11bc52813a6630b4ce114cc58a9ead8a4bcb85
Nanapas Baola <nanapasb67@nu.ac.th>
2026-07-01 11:12:13 +0700

    done

---
+++ lab2_exercise5.dart
@@ -0,0 +1,30 @@
+void printGrade(int score) {
+  print('Score: $score');
+  if (score>=80){
+    print('Grade A');
+  } else if (score>=70){
+    print('Grade B');
+  } else if (score>=60){
+    print('Grade C');
+    }else if (score>=50){
+    print('Grade D');
+    }else {
+      print('Grade F');
+    }
+}
+void main(){
+  printGrade(85);
+  printGrade(72);
+  printGrade(50);
+  printGrade(40);
+
+  print('Numbers 1 to 10:');
+  for (int i = 1; i <= 10; i++) {
+    print(i);
+  }
+var fruits = ['Apple', 'Banana', 'Cherry'];
+  print('Fruits:');
+  for (var fruit in fruits) {
+    print('- $fruit');
+  }
+}
