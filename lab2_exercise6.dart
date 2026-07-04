1f47d4a2884fd1456b845eb81e87f3bf850d5843
Nanapas Baola <nanapasb67@nu.ac.th>
2026-07-04 21:03:48 +0700

    done

--- lab2_exercise6.dart
+++ lab2_exercise6.dart
@@ -5,10 +5,10 @@ double triangle(double base, double height) => 0.5 * base * height;
   double cylinder(double  radius , double height) => 3.14*(radius*radius)*height;
   
   void main() {
-    var triArea = triangle(127, 170);
+    double triArea = triangle(127, 170);
     print('triangle area: $triArea');
-    var rectArea = rectangle(127, 97);
+    double rectArea = rectangle(127, 97);
     print('Rectangle area : $rectArea');
-    var cyl = cylinder(7, 127);
+    double cyl = cylinder(7, 127);
     print('Volume of a cylinder: $cyl');
   }
